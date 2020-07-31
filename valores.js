826676da1181ba577bea93677aba1ad85af46463
CurtoBrull <curto.brull.javier@gmail.com>
2020-07-31 20:31:45 +0200

    primeras clases

---
+++ valores.js
@@ -0,0 +1,23 @@
+// Primitivos
+
+// Number
+40
+
+// String
+"Javier Curto"
+
+// booleano
+true
+false
+
+// Empty value
+null
+undefined
+
+// No Primitivos
+
+// Array
+[1,2,3]
+{ nombre: "Javier Curto"}
+
+// Comprobar con typeof
---
+++ variables.js
@@ -0,0 +1,15 @@
+var nombre = "Javier";
+
+// Declarar
+var edad;
+
+// Inicializar
+
+edad = 41;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+  nombre: "Javier",
+  edad: 41
+}
